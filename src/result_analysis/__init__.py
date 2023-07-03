d71b24fcde5b475c32bbfc20a310ffe900dfeb5d
paumunoz <paulam@uma.es>
2023-07-03 16:59:50 +0200

    [Packaging] New functions added.

--- src/result_analysis/__init__.py
+++ src/result_analysis/__init__.py
@@ -1,4 +1,5 @@
 from .alignment_graphic import generate_alignment_graphic
+from .gap_tunning import get_change_point, execute_regression
 from .statistical_graphics import generate_parallel_behavior_graphic, \
     generate_statistical_info_stairs, \
     generate_statistical_info_stairs_comparison, \
@@ -8,4 +9,6 @@ __all__ = ['generate_alignment_graphic',
            'generate_parallel_behavior_graphic',
            'generate_statistical_info_stairs_variability',
            'generate_statistical_info_stairs_comparison',
-           'generate_statistical_info_stairs']
+           'generate_statistical_info_stairs',
+           'get_change_point',
+           'execute_regression']
--- src/util/__init__.py
+++ src/util/__init__.py
@@ -1,6 +1,7 @@
-from dataframe_util import clean_df
-from file_util import list_directory_files, flatten_dictionary, get_property_values, get_property_methods
-from float_util import get_input_values_list, max_tolerance
+from .dataframe_util import clean_df
+from .file_util import list_directory_files, flatten_dictionary, get_property_values, get_property_methods, \
+    generate_sublist
+from .float_util import get_input_values_list, max_tolerance
 
 __all__ = ['clean_df',
            'list_directory_files',
@@ -8,4 +9,5 @@ __all__ = ['clean_df',
            'get_property_values',
            'get_property_methods',
            'get_input_values_list',
-           'max_tolerance']
+           'max_tolerance',
+           'generate_sublist']
