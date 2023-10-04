d85a8a1240a823668fc6e2b8cb07d30729a330fc
paumunoz <paulam@uma.es>
2023-10-04 15:17:31 +0200

    [Packages] __all__ updated to include classes.

--- src/algorithm/__init__.py
+++ src/algorithm/__init__.py
@@ -2,6 +2,6 @@ from .needleman_wunsch_affine_gap import NeedlemanWunschAffineGap
 from .needleman_wunsch_base import NeedlemanWunschBase
 from .needleman_wunsch_tolerance import NeedlemanWunschTolerance
 
-__all__ = ["needleman_wunsch_base",
-           "needleman_wunsch_tolerance",
-           "needleman_wunsch_affine_gap"]
+__all__ = ["NeedlemanWunschBase",
+           "NeedlemanWunschTolerance",
+           "NeedlemanWunschAffineGap"]
