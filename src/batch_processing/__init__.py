4654ec8deaaebda53fe1af8063e0779262d6a1e8
paumunoz <paulam@uma.es>
2023-11-07 15:53:59 +0100

    [Factory methods] Updated to make it more general to NDW, DTW and LCSS

---
+++ src/batch_processing/__init__.py
@@ -0,0 +1,9 @@
+from batch_processing.alg_config.alignment_config import AlignmentConfiguration
+from .algorithm_factory import AlignmentAlgorithmFactory
+from .analysis_factory import AnalysisFactory
+from .config_factory import ConfigFactory
+
+__all__ = ['AlignmentAlgorithmFactory',
+           'AlignmentConfiguration',
+           'AnalysisFactory',
+           'ConfigFactory']
