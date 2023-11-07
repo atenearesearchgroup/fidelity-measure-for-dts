56b6ce78a58ba11f943eb8caafb4713adf7e4d49
paumunoz <paulam@uma.es>
2023-11-07 14:14:11 +0100

    [float-util] min_tolerance function added to compare floats with a tolerance value.

--- src/util/float_util.py
+++ src/util/float_util.py
@@ -1,7 +1,8 @@
 import itertools
 
 
-def max_tolerance(v1: float, v2: float, v3: float, equals_value: float, tolerance: float = 0.0001) -> (float, int):
+def max_tolerance(v1: float, v2: float, v3: float, equals_value: float,
+                  tolerance: float = 0.0001) -> (float, int):
     """Comparison of three floats considering a certain tolerance.
         Returns
         --------
@@ -21,6 +22,29 @@ def max_tolerance(v1: float, v2: float, v3: float, equals_value: float, toleranc
         max_value = v2
         max_index = 1
     return max_value, max_index
+
+
+def min_tolerance(v1: float, v2: float, v3: float,
+                  tolerance: float = 0.0001) -> (float, int):
+    """Comparison of three floats considering a certain tolerance.
+        Returns
+        --------
+        Min value and its position
+    """
+    if v1 <= (v3 - tolerance):
+        if v1 <= (v2 - tolerance):
+            min_value = v1
+            min_index = 1
+        else:
+            min_value = v2
+            min_index = 2
+    elif v3 <= (v2 - tolerance):
+        min_value = v3
+        min_index = 3
+    else:
+        min_value = v2
+        min_index = 3
+    return min_value, min_index
 
 
 def get_input_values_list(*args):
