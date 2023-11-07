bc5190ac9719d3c3aac5aa267d69ce3f1d4a5b5f
paumunoz <paulam@uma.es>
2023-11-07 15:55:22 +0100

    [Algorithm] Added abstract score function.

--- src/algorithm/__init__.py
+++ src/algorithm/__init__.py
@@ -1,7 +1,21 @@
-from .needleman_wunsch_affine_gap import NeedlemanWunschAffineGap
-from .needleman_wunsch_base import NeedlemanWunschBase
-from .needleman_wunsch_tolerance import NeedlemanWunschTolerance
+from algorithm.dtw.dynamic_time_warping_base import DynamicTimeWarpingBase
+from algorithm.dtw.dynamic_time_warping_lug import DynamicTimeWarpingLugaresi
+from algorithm.dtw.dynamic_time_warping_snaps import DynamicTimeWarpingSnaps
+from algorithm.lcss.longes_common_subsequence_events import LongestCommonSubsequenceEvents
+from algorithm.lcss.longes_common_subsequence_kpi import LongestCommonSubsequenceKPI
+from algorithm.lcss.longest_common_subsequence_base import LongestCommonSubsequenceBase
+from algorithm.ndw.needleman_wunsch_affine_gap import NeedlemanWunschAffineGap
+from algorithm.ndw.needleman_wunsch_base import NeedlemanWunschBase
+from algorithm.ndw.needleman_wunsch_tolerance import NeedlemanWunschTolerance
+from .alignment_algorithm import AlignmentAlgorithm
 
 __all__ = ["NeedlemanWunschBase",
            "NeedlemanWunschTolerance",
-           "NeedlemanWunschAffineGap"]
+           "NeedlemanWunschAffineGap",
+           "AlignmentAlgorithm",
+           "DynamicTimeWarpingLugaresi",
+           "DynamicTimeWarpingSnaps",
+           "DynamicTimeWarpingBase",
+           "LongestCommonSubsequenceKPI",
+           "LongestCommonSubsequenceBase",
+           "LongestCommonSubsequenceEvents"]
