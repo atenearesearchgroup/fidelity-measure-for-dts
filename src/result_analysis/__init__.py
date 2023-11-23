f1ea939b12bc5e192725e2ce2656185cf22dcbec
paumunoz <paulam@uma.es>
2023-11-23 12:53:23 +0100

    [Modules] Added new graphics Classes.

--- src/result_analysis/__init__.py
+++ src/result_analysis/__init__.py
@@ -1,11 +1,16 @@
-from .alignment_graphic import generate_alignment_graphic
+from result_analysis.alignment_graphic.alignment_graphic import AlignmentGraphics
+from result_analysis.alignment_graphic.dtw_alignment_graphic import \
+    DynamicTimeWarpingAlignmentGraphics
+from result_analysis.alignment_graphic.ndw_alignment_graphic import NeedlemanWunschAlignmentGraphics
 from .gap_tunning import get_change_point, execute_regression
 from .statistical_graphics import generate_parallel_behavior_graphic, \
     generate_statistical_info_stairs, \
     generate_statistical_info_stairs_comparison, \
     generate_statistical_info_stairs_variability
 
-__all__ = ['generate_alignment_graphic',
+__all__ = ['AlignmentGraphics',
+           'NeedlemanWunschAlignmentGraphics',
+           'DynamicTimeWarpingAlignmentGraphics',
            'generate_parallel_behavior_graphic',
            'generate_statistical_info_stairs_variability',
            'generate_statistical_info_stairs_comparison',
