06dedb0df40eff5974cf76ffffc4804070345657
paumunoz <paulam@uma.es>
2023-11-23 12:51:59 +0100

    [DTW] Added additional metrics to analyse DTW results.

--- src/metrics/dtw/dtw_alignment.py
+++ src/metrics/dtw/dtw_alignment.py
@@ -1,5 +1,3 @@
-import numpy as np
-
 from metrics.alignment_base import AlignmentBase
 
 
@@ -14,13 +12,33 @@ class DynamicTimeWarpingAlignmentMetrics(AlignmentBase):
 
 
 class DynamicTimeWarpingSnapsAlignmentMetrics(AlignmentBase):
+    def _number_one_to_many(self, prefix, trace):
+        column_name = prefix + self._timestamp_label
+        return len(self._alignment[column_name]) - len(trace)
+
     @property
-    def percentage_one_to_many(self):
-        params = [prefix + self._timestamp_label for prefix in [self.PREFIX_PT, self.PREFIX_PT]]
-        n_values = 0
-        for p in params:
-            n_values += len(self._alignment) - self._alignment[p].value_counts().index.nunique()
-        return n_values / np.max([len(self._dt_trace), len(self._pt_trace)])
+    def number_one_to_many_pt(self):
+        return self._number_one_to_many(self.PREFIX_PT, self._pt_trace)
+
+    @property
+    def number_one_to_many_dt(self):
+        return self._number_one_to_many(self.PREFIX_DT, self._dt_trace)
+
+    def _times_a_snap_is_aligned(self, prefix):
+        column_name = prefix + self._timestamp_label
+        repeated_values_count = self._alignment[column_name].value_counts()
+        return {f'max_rep': repeated_values_count.max(),
+                f'number_rep': len(repeated_values_count[repeated_values_count > 1]),
+                f'avg_rep': repeated_values_count[repeated_values_count > 1].mean(),
+                f'std_rep': repeated_values_count[repeated_values_count > 1].std()}
+
+    @property
+    def pt_snap_aligned(self):
+        return self._times_a_snap_is_aligned(self.PREFIX_PT)
+
+    @property
+    def dt_snap_aligned(self):
+        return self._times_a_snap_is_aligned(self.PREFIX_DT)
 
     @property
     def score(self) -> float:
