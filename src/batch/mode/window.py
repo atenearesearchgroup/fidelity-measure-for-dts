f8944c458b1e318c61a1c5caca1b73f9409b7ec6
paumunoz <paulam@uma.es>
2024-05-06 10:49:32 +0200

    [Window] Prevent recalculation of already calculated configurations.

--- src/batch/mode/window.py
+++ src/batch/mode/window.py
@@ -24,40 +24,43 @@ class WindowAlignmentMode(AlignmentMode):
         self._set_intervals()
 
         w_range = self._get_range(self._execution.dt_trace, self._execution.pt_trace)
-        for w_end in w_range:
-            dt_trace_w, pt_trace_w = self._get_subtrace(w_end,
-                                                        self._execution.dt_trace,
-                                                        self._execution.pt_trace)
+        if not os.path.isfile(self._get_metrics_filepath('.csv')):
+            for w_end in w_range:
+                dt_trace_w, pt_trace_w = self._get_subtrace(w_end,
+                                                            self._execution.dt_trace,
+                                                            self._execution.pt_trace)
 
-            alignment_df, score, time_dict = self._execute_algorithm(dt_trace_w,
-                                                                     pt_trace_w)
+                alignment_df, score, time_dict = self._execute_algorithm(dt_trace_w,
+                                                                         pt_trace_w)
 
-            time_window_dict = self._get_time_window(dt_trace_w, w_end)
-            if self._config.align_files:
-                self._export_alignment_results(alignment_df,
-                                               dt_trace_w,
-                                               pt_trace_w,
-                                               os.path.join(self._execution.folder,
-                                                            self._execution.get_filename(
-                                                                extra=f"({time_window_dict[self.TS_START]},"
-                                                                      f"{time_window_dict[self.TS_END]})")))
+                time_window_dict = self._get_time_window(dt_trace_w, w_end)
+                if self._config.align_files:
+                    self._export_alignment_results(alignment_df,
+                                                   dt_trace_w,
+                                                   pt_trace_w,
+                                                   os.path.join(self._execution.folder,
+                                                                self._execution.get_filename(
+                                                                    extra=f"({time_window_dict[self.TS_START]},"
+                                                                          f"{time_window_dict[self.TS_END]})")))
 
-            self._execution.calculate_metrics(score, alignment_df, dt_trace_w, pt_trace_w,
-                                              time_dict,
-                                              extra_columns=time_window_dict)
+                self._execution.calculate_metrics(score, alignment_df, dt_trace_w, pt_trace_w,
+                                                  time_dict,
+                                                  extra_columns=time_window_dict)
 
-        if w_range.shape[0] > 0:
-            self._execution.export_metrics(self._get_metrics_filepath('.csv'))
+            if w_range.shape[0] > 0:
+                self._execution.export_metrics(self._get_metrics_filepath('.csv'))
 
-            fig = generate_window_statistics(self._execution.dt_trace,
-                                             self._execution.pt_trace,
-                                             self._execution.results,
-                                             self._config.param_interest,
-                                             self._config.timestamp_label,
-                                             self._execution.alg_current_config['mad'][
-                                                 self._config.param_interest])
-            fig.write_image(self._get_metrics_filepath('.pdf'), format="pdf",
-                            engine=self._config.engine)
+                fig = generate_window_statistics(self._execution.dt_trace,
+                                                 self._execution.pt_trace,
+                                                 self._execution.results,
+                                                 self._config.param_interest,
+                                                 self._config.timestamp_label,
+                                                 self._execution.alg_current_config['mad'][
+                                                     self._config.param_interest])
+                fig.write_image(self._get_metrics_filepath('.pdf'), format="pdf",
+                                engine=self._config.engine)
+        else:
+            print(f"Config already computed: {self._get_metrics_filepath('')}")
 
     def _get_time_window(self, dt_trace_w, w_end):
         return {
@@ -88,4 +91,4 @@ class WindowAlignmentMode(AlignmentMode):
 
     def _get_subtrace(self, w_end, dt_trace, pt_trace):
         w_start = w_end - self._interval_duration
-        return dt_trace.iloc[w_start:w_end], pt_trace.iloc[w_start:w_end]
+        return dt_trace.loc[w_start:w_end], pt_trace.loc[w_start:w_end]
